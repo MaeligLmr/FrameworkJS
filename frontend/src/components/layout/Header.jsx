99f2a086881bc6c6766dda994dcc0739495a0ff6
Lamarre <maelig.lamarre49@gmail.com>
2025-12-04 13:12:34 +0100

    (chore) front : created frontend files

---
+++ frontend/src/components/layout/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="w-full bg-white/80 dark:bg-gray-800/80 shadow-sm">
+    <div className="max-w-4xl mx-auto p-4">Header</div>
+  </header>
+);
+
+export default Header;
