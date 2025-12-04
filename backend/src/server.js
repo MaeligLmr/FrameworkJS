4599aaa473c4bceca1671883f80b279b2e96f746
Lamarre <maelig.lamarre49@gmail.com>
2025-12-04 15:52:39 +0100

    (fix) back : added cors authorization

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import { connectDB } from './config/database.js';
 import { router as articleRoutes } from './routes/article.js';
 import { router as authRoutes } from './routes/auth.js';
 import { router as commentRoutes } from './routes/comment.js';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
@@ -20,6 +21,17 @@ app.get('/', (req, res) => {
     });
 });
 
+app.use(cors({
+    origin: function (origin, callback) {
+        const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [];
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = `L'origine CORS ${origin} n'est pas autorisée.`;
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 app.use('/api/articles', articleRoutes);
 app.use('/api/auth', authRoutes);
